16e614878be8db8108ba63fdb9d72629385c4518
avveshka <aleksandr_androsov@mail.ru>
2019-08-05 12:09:37 +0300

    fix collision

--- Tanks/Tanks/Model/Objects/MovableGameObject.cs
+++ Tanks/Tanks/Model/Objects/MovableGameObject.cs
@@ -23,6 +23,22 @@ namespace Model.Objects
 
         public void ChangeDirection(direction dir)
         {
+            if(ObjectDirection == direction.Left)
+            {
+                x += 1;
+            }
+            else if (ObjectDirection == direction.Right)
+            {
+                x -= 1;
+            }
+            else if (ObjectDirection == direction.Up)
+            {
+                y += 1;
+            }
+            else if (ObjectDirection == direction.Down)
+            {
+                y -= 1;
+            }
             ObjectDirection = dir;
         }
 
--- Tanks/Tanks/view/MainForm.Designer.cs
+++ Tanks/Tanks/view/MainForm.Designer.cs
@@ -60,8 +60,8 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.imgMap);
